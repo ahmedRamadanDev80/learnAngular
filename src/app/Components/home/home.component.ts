9848594ce579043dc468eeb90dce7c5bbe22221c
altair <ar80pro@gmail.com>
2023-05-10 00:30:14 +0200

    RxJS - creating observables

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PromotionAdsService } from 'src/app/Services/promotion-ads.service';
 import { StoreData } from 'src/app/ViewModels/store-data';
 
 @Component({
@@ -6,11 +8,29 @@ import { StoreData } from 'src/app/ViewModels/store-data';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-storeInfo:StoreData;
-isImgShown:boolean=true;
-constructor(){
+export class HomeComponent implements OnInit,OnDestroy{
+  private subscriptions:Subscription[] =[];
+  storeInfo:StoreData;
+  isImgShown:boolean=true;
+constructor(private promoAd:PromotionAdsService){
   this.storeInfo = new StoreData('altair store','https://picsum.photos/200/200',['cairo','alex','beheira']);
 }
+ 
+  ngOnInit(): void {
+    let observer = {
+      next:(data:string)=>{ console.log(data); },
+      error:(err:string)=>{ console.log(err);  },
+      complete:()=>{ console.log('ads complete.'); }
+    };
+
+    this.subscriptions.push( this.promoAd.getScheduledAds(3).subscribe(observer));
+   
+  }
 toggleImg(){this.isImgShown = !this.isImgShown}
+
+ngOnDestroy(): void {
+  for(let sub of this.subscriptions){
+    sub.unsubscribe();
+  }
+}
 }
---
+++ src/app/Services/promotion-ads.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PromotionAdsService {
+  private adsList:string[];
+  constructor() { 
+    this.adsList = ['big discount'
+                    ,'sale up to 50%'
+                    ,'black friday offers'
+                    ,'wabba labba dub dub'
+                    ,'special white firiday 70% sale offer'];
+  }
+
+  getScheduledAds(intervalInSeconds:number):Observable<string>
+  {
+    return new Observable<string>((observer)=>{
+      // observer.next();
+      // observer.error();
+      // observer.complete();
+      let counter =0;
+      let adsTimer = setInterval(()=>{
+        if(counter == this.adsList.length){
+          observer.complete();
+        }
+        if(this.adsList[counter]==''){
+          observer.error("Error: Empty Ad ! ");
+        }
+        observer.next(this.adsList[counter]);
+        counter++;
+
+      },intervalInSeconds*1000);
+      return { 
+        unsubscribe(){
+          clearInterval(adsTimer);
+        }
+      }
+    });
+  }
+}
